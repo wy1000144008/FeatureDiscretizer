c5020f71d6b9ce1b0b81503cac0f18f8ce888daa
王叶 <wy694141134@gmail.com>
2019-12-19 16:59:05 +0900

    fix sort

--- feature_discretizer.h
+++ feature_discretizer.h
@@ -5,9 +5,11 @@
 
 int main() 
 {
+	//para 1 bin_num, 2 feature_num, 3 dataset path, 4 delimiter, 5 is skip head
 	Dataset ds(5,"raw/bank-note-exp.txt",",",true); 
 	ds.init();
 	ds.printInfo();
+	ds.make_bins(5);
     std::cout << "Test"  << std::endl; 
     return 0;
 }
